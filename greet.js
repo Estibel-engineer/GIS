a9f480df7ce4a1a3355ebeefc390ab335143f2fd
Estibel-engineer <esteveman3@gmail.com>
2025-04-16 11:16:42 +0300

    the sixth version

--- greet.js
+++ greet.js
@@ -1,6 +1,7 @@
 function greet(name = "stranger") {
   const now = new Date().toLocaleTimeString();
   console.log(`[${now}] Hello, ${name}!`);
+  console.log("Git is aweseome!");
 }
 
 module.exports = greet;
